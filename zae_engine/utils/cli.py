fe538e1cdbbac10725029a3cde37da4848379054
zae-park <tom941105@gmail.com>
2024-02-06 18:14:36 +0900

    feat: - add new click command to create example_script.py - add example_script.py

--- zae_engine/utils/cli.py
+++ zae_engine/utils/cli.py
@@ -1,11 +1,16 @@
+import shutil
+
 import click
+
 from .tree_builder import TreeBuilder
 
 
 @click.command()
-@click.argument('feature')
+@click.argument("feature", "path")
 def cli_run(feature: str):
-    if feature == 'tree':
-        TreeBuilder.print_tree('./')
+    if feature == "tree":
+        TreeBuilder.print_tree("./")
+    if feature in ["example", "sample", "snippet", "ex"]:
+        shutil.copy("./example_script.py", path)
     else:
         print(f'Invalid command "{feature}".')
