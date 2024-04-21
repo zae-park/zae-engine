72de5b809d97ad2db9ae71edd9ad9f8513e57d95
zae-park <tom941105@gmail.com>
2024-04-21 17:31:24 +0900

    fix relative import

--- zae_engine/inference/beat.py
+++ zae_engine/inference/beat.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 
 from ..models import beat_segmentation
 from ..trainer import Trainer
-from ...data_pipeline.collate import BeatCollateSeq as Col
+from ..data_pipeline.collate import BeatCollateSeq as Col
 
 
 def core(x: Union[np.ndarray, torch.Tensor]):
