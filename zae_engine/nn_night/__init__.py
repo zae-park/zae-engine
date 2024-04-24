f961c63d432b365d6b71b9d4a93cb9211f4819ac
zae-park <tom941105@gmail.com>
2024-04-24 09:38:03 +0900

    feat: add Residual module

--- zae_engine/nn_night/__init__.py
+++ zae_engine/nn_night/__init__.py
@@ -1,3 +1,3 @@
 from ._activation import *  # noqa: F403
-from ._layer import *       # noqa: F403
-from ._module import *      # noqa: F403
+from .layers import *  # noqa: F403
+from ._module import *  # noqa: F403
--- zae_engine/nn_night/layers/__init__.py
+++ zae_engine/nn_night/layers/__init__.py
@@ -1,1 +1,2 @@
 from involution import Inv1d
+from residual_connection import Residual
