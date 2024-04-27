a9941614649ee25a004a2bfedd3355afc185fca9
zae-park <tom941105@gmail.com>
2024-04-28 00:43:33 +0900

    feat: - optimize find_layers - add dim_correction method (optimized combination of reduce_dim & expand_dim) - add apply_new_dict method to change model's layer - refine convert method

--- zae_engine/models/utils/dim_converter.py
+++ zae_engine/models/utils/dim_converter.py
@@ -25,72 +25,42 @@ class DimConverter:
     def __init__(self, model: nn.Module):
         self.model = model
         # finding dimension-convertable layers
-        self.layers = self.find_layers(model)
+        self.layer_dict, self.param_dict = self.find_layers(model)
 
-    @staticmethod
-    def dim_analysis(model: nn.Module) -> int:
-        # print(model)
-        return 1
-
-    def find_layers(self, model: nn.Module) -> defaultdict[str, dict[str, None | Module | torch.Tensor]]:
+    def find_layers(self, model: nn.Module) -> tuple[dict, dict]:
         """
         find dimension convertable layers in given model.
         return dictionary which has 'layer path' as key, and tuple of layer api and weight tensor as value.
         :param model:
-        :return: Dict[str, tuple[nn.Module, torch.Tensor]]
+        :return: tuple[dict, dict]
         """
-        layer_dict = defaultdict(lambda: {"api": None, "weight": None, "bias": None})
+        layer_dict = {}
+        param_dict = {}
 
         for name, weight in model.named_parameters():
-            name_split = name.split(".")
-            if name_split[-1] not in ["weight", "bias"]:
-                module_path, value_type = name, ""
-            else:
-                module_path, value_type = ".".join(name_split[:-1]), name_split[-1]
+            param_dict[name] = weight
+            if name.endswith(("weight", "bias")):
+                root, leaf = name.rsplit(".", 1)
+                module = model.get_submodule(root)
+                if isinstance(module, self.convertable):
+                    layer_dict[root] = module
 
-            module: Module = model.get_submodule(module_path)
-            if isinstance(module, self.convertable):
-                layer_dict[module_path]["api"] = module
-                layer_dict[module_path][value_type] = weight
+        return layer_dict, param_dict
 
-        return layer_dict
+    def dim_correction(self, reduce: bool):
+        correction_map = {nn.Conv2d: nn.Conv3d, nn.Conv1d: nn.Conv2d}  # default is expand mode
+        if reduce:
+            correction_map = {v: k for k, v in correction_map.items()}
 
-    def expand_dim(
-        self, src_dict: dict[str, Union[nn.Module, torch.Tensor]]
-    ) -> dict[str, Union[nn.Module, torch.Tensor]]:
-        expand_map = {nn.Conv2d: nn.Conv3d, nn.Conv1d: nn.Conv2d}
-        dst_dict = {}
-        for k, v in src_dict.items():
-            for kk, vv in v.items():
-                if kk == "weight":
-                    dst_dict[f"{k}.{kk}"] = torch.mm(vv.unsqueeze(-1), vv.unsqueeze(-2))
-                elif kk == "bias":
-                    dst_dict[f"{k}.{kk}"] = vv
-                else:
-                    dst_api = expand_map[type(vv)]
-                    needs = dst_api.__init__.__annotations__
-                    ready = {k: v for k, v in self.const_getter(vv, reduce=False).items() if k in needs.keys()}
-                    dst_dict[k] = dst_api(**ready)
-        return dst_dict
+        for k, v in self.param_dict.items():
+            if k.endswith("weight"):
+                self.param_dict[k] = v.mean(-1) if reduce else torch.mm(v.unsqueeze(-1), v.unsqueeze(-2))
 
-    def reduce_dim(
-        self, src_dict: dict[str, Union[nn.Module, torch.Tensor]]
-    ) -> dict[str, Union[nn.Module, torch.Tensor]]:
-        reduce_map = {nn.Conv3d: nn.Conv2d, nn.Conv2d: nn.Conv1d}
-
-        dst_dict = {}
-        for k, v in src_dict.items():
-            for kk, vv in v.items():
-                if kk == "weight":
-                    dst_dict[f"{k}.{kk}"] = vv.mean(-1)
-                elif kk == "bias":
-                    dst_dict[f"{k}.{kk}"] = vv
-                else:
-                    dst_api = reduce_map[type(vv)]
-                    needs = dst_api.__init__.__annotations__
-                    ready = {k: v for k, v in self.const_getter(vv, reduce=True).items() if k in needs.keys()}
-                    dst_dict[k] = dst_api(**ready)
-        return dst_dict
+        for k, v in self.layer_dict.items():
+            corrected_layer = correction_map[type(v)]
+            needs = corrected_layer.__init__.__annotations__
+            ready = {k: v for k, v in self.const_getter(v, reduce=reduce).items() if k in needs.keys()}
+            self.layer_dict[k] = corrected_layer(**ready)
 
     @staticmethod
     def const_getter(conv_module: nn.Module, reduce: bool):
@@ -104,6 +74,13 @@ class DimConverter:
             const[k] = v
         return const
 
+    def apply_new_dict(self, base_module: nn.Module, name: str, module: nn.Module):
+        n = name.split(".")
+        if len(n) == 1:
+            base_module.add_module(name, module)
+        else:
+            self.apply_new_dict(base_module=base_module.get_submodule(n[0]), name=".".join(n[1:]), module=module)
+
     def convert(self, pattern: str, *args, **kwargs):
         # 0. compare model's dimension with request pattern
         left, right = pattern.split("->")
@@ -112,12 +89,14 @@ class DimConverter:
         right = right.strip()
 
         new_model = deepcopy(self.model)
-
         if left == right:
             return new_model
 
         # convert dim of layers to appropriately
-        new_dict = self.reduce_dim(self.layers) if left > right else self.expand_dim(self.layers)
+        self.dim_correction(reduce=left > right)
+
+        for k, v in self.layer_dict.items():
+            self.apply_new_dict(new_model, k, v)
 
         # 2. apply them to new model
         new_model.load_state_dict(new_dict)
