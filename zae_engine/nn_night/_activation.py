4dbe5a33d0067cca4866c5ad4941616213d035ab
zae-park <tom941105@gmail.com>
2024-04-30 15:19:40 +0900

    feat: add assertion in ClippedReLU

--- zae_engine/nn_night/_activation.py
+++ zae_engine/nn_night/_activation.py
@@ -10,8 +10,13 @@ class ClippedReLU(nn.Module):
     :param lower: float
         The lower threshold. default is 0.
     """
+
     def __init__(self, upper: float = 1.0, lower: float = 0.0):
         super(ClippedReLU, self).__init__()
+        assert upper > lower, (
+            f'Expect 0th argument "upper" greater than 1st argument "lower".'
+            f" But upper({upper:.2f}) is less than lower ({lower:.2f})"
+        )
         self.upper = upper
         self.lower = lower
         self.act = nn.ReLU()
