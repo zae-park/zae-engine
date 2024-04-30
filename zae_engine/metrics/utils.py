dc5c7c50b6b560357bcfa5c0b021bfc909a6a952
zae-park <tom941105@gmail.com>
2024-05-01 00:14:37 +0900

    optimize: allow multiple args and expand type coverage for utils

--- zae_engine/metrics/utils.py
+++ zae_engine/metrics/utils.py
@@ -1,11 +1,17 @@
+from typing import Union, Iterable
 import numpy as np
 import torch
 
 
 def np2torch(dtype: torch.dtype):
     def deco(func):
-        def wrapper(x: np.ndarray | torch.Tensor):
-            x = torch.tensor(x, dtype=dtype) if isinstance(x, np.ndarray) else x.astype(dtype)
-            return func(x)
+        def wrapper(*args: Union[np.ndarray, torch.Tensor, bool, int, float]):
+
+            if len(args) == 1:
+                args = a if isinstance((a := args[0]), torch.Tensor) else torch.tensor(a, dtype=dtype)
+                return func(args)
+            else:
+                args = [a if isinstance(a, torch.Tensor) else torch.tensor(a, dtype=dtype) for a in args]
+                return func(*args)
         return wrapper
     return deco
