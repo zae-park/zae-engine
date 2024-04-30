bec7337296be622bfe287149124cc7ffc99f8487
zae-park <tom941105@gmail.com>
2024-04-30 16:58:56 +0900

    test: add assertion for involution

--- tests/nn_night/layers/test_involution.py
+++ tests/nn_night/layers/test_involution.py
@@ -15,32 +15,28 @@ class TestInv1d(unittest.TestCase):
         pass
 
     def setUp(self) -> None:
-        pass
+        self.ch = 16
+        self.stride = 1
+        self.group = 2
 
     def tearDown(self) -> None:
         pass
-
-    def test_shape(self):
-        random_ch = randint(1, 32)
-        random_dim = randint(3, 2048)
-        sample = torch.zeros(1, random_ch, random_dim)
-        inv_layer = Inv1d(random_ch, num_groups=2, kernel_size=3, stride=1, reduction_ratio=3)
-        out = inv_layer(sample)
-        kernel_size, stride = inv_layer.kernel_size, inv_layer.stride
-        output_size = np.floor((random_dim - kernel_size + 2 * (kernel_size - 1) // 2) / stride + 1)
-        self.assertEqual(out.size()[-1], output_size)
 
     def test_divide(self):
         random_ch = randint(1, 32)
         random_dim = randint(3, 2048)
         random_stride = randint(1, 5)
         sample = torch.zeros(1, random_ch, random_dim)
-        inv_layer = Inv1d(random_ch, num_groups=2, kernel_size=3, stride=random_stride, reduction_ratio=3)
-        if random_dim % random_stride:
+        if random_ch % self.group:
             with self.assertRaises(AssertionError):
-                inv_layer(sample)
+                inv = Inv1d(random_ch, num_groups=self.group, kernel_size=3, stride=random_stride, reduction_ratio=3)
         else:
-            inv_layer(sample)
+            inv = Inv1d(random_ch, num_groups=self.group, kernel_size=3, stride=random_stride, reduction_ratio=3)
+            if random_dim % random_stride:
+                with self.assertRaises(AssertionError):
+                    inv(sample)
+            else:
+                inv(sample)
 
     def test_num_groups(self):
         random_ch = randint(1, 32)
