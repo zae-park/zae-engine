defb86b802bb14841e283ae2885ba27634d1949d
zae-park <tom941105@gmail.com>
2024-05-01 22:56:39 +0900

    add initial test cases for losses

---
+++ tests/loss/test_iou.py
@@ -0,0 +1,20 @@
+import unittest
+
+import numpy as np
+
+from zae_engine.loss.iou import miou, giou
+
+
+class TestLossIoU(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_mIoU(self):
+        pass
+
+    def test_gIoU(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
