9c5d90825a7944225e6780da46a41ab1ab15fb8b
zae-park <tom941105@gmail.com>
2024-05-15 18:15:41 +0900

    optimize test case

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -88,8 +88,11 @@ class TestTrainer(unittest.TestCase):
         # test _scheduler_step_check
         pass
         # test logging
-        self.assertEqual(len(self.loader), len(self.trainer.log_train["loss"]))
-        self.assertEqual(0, len(self.trainer.log_test["loss"]))
+        log_a, log_b = self.trainer.log_train, self.trainer.log_test
+        if self.trainer.mode == "test":
+            log_a, log_b = log_b, log_a
+        self.assertEqual(len(self.loader), len(log_a["loss"]))
+        self.assertEqual(0, len(log_b["loss"]))
 
         # test batch step
         self.trainer.scheduler_step_on_batch = True
