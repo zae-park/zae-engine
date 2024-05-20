259c0b9e57e0609c9e4f9d13f923c1e82b301e83
zae-park <tom941105@gmail.com>
2024-05-20 19:44:59 +0900

    test: add initial test case for unet foundation model

---
+++ tests/models/foundations/test_unet.py
@@ -0,0 +1,32 @@
+import unittest
+
+import torch
+
+from zae_engine.models.foundations import unet
+
+
+class TestUnet(unittest.TestCase):
+    test_sample = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # cls.test_sample = torch.randn((2, torch.randint(1, 16, size=[1]), 256, 256))
+        pass
+
+    def setUp(self) -> None:
+        # self.test_sample = torch.randn((2, torch.randint(1, 16, size=[1]), 256, 256))
+        self.test_sample = torch.randn((2, 3, 256, 256))
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_unet(self):
+        model = unet()
+        output = model(self.test_sample)
+        expect_output = self.test_sample.mean(1, keepdim=True)
+
+        self.assertEqual(output.shape, expect_output.shape)
+
+
+if __name__ == "__main__":
+    unittest.main()
