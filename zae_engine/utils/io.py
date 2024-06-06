f0d18dd3166e5d0c7fa71d979b74691fd094071f
zae-park <tom941105@gmail.com>
2024-06-06 12:14:44 +0900

    rewriting example_ecg docstring

--- zae_engine/utils/io.py
+++ zae_engine/utils/io.py
@@ -31,18 +31,37 @@ def image_from_url(url: str, save_dst: str = None) -> Union[None, Image.Image]:
 
 def example_ecg(beat_idx: int = None) -> Tuple[np.ndarray, ...]:
     """
-    Load 10 second ecg recording & annotation from example data with sampling frequency 250Hz.
-    The example data from LUDB (https://www.physionet.org/content/ludb/1.0.1).
-    The '*.zea' file includes QRS complex information.
-    If argument 'beat_idx' is None(default), return the 10 second recording and label sequence.
-    If not, return the recording, r-peak index, and beat type for beat_idx'th beat.
-    :param beat_idx: int, optional
-    The index of beat in data.
-    Note that the value cannot be more than maximum index of beats in data (12).
-    If this parameter is not specified, run for 10 second data.
-    :return:
-    If beat_idx was given, return recording, r-peak index, and beat type for beat_idx'th beat.
-    If not, return recording, label sequence for 10 sec data.
+    Load a 10-second ECG recording and annotation from LUDB dataset [1], which has a sampling frequency of 250Hz.
+
+    The '*.zae' file in package contains QRS complex information from raw data[1] and beat locations via algorithm.
+    If the argument 'beat_idx' is None (default), the function returns the 10-second recording and label sequence.
+    If 'beat_idx' is specified, the function returns the segment of the recording, the location, and the symbol of beat.
+
+    Parameters
+    ----------
+    beat_idx : int, optional
+        The index of the beat in the data.
+        The value cannot be greater than the maximum index of beats in the data (12).
+        If this parameter is not specified, the function returns the 10-second data.
+
+    Returns
+    -------
+    signal : np.ndarray
+        The 10-second ECG recording.
+        If 'beat_idx' is specified, this is the segment of the recording around the specified beat.
+    label : np.ndarray or int
+        If 'beat_idx' is None, this is the label sequence for the 10-second data.
+        If 'beat_idx' is specified, this is the type of the specified beat.
+    loc : None or int
+        The location of the specified beat if 'beat_idx' is specified.
+        Otherwise, this is None.
+
+    References
+    ----------
+    .. [1]  Kalyakulina, A., Yusipov, I., Moskalenko, V., et al.
+            Lobachevsky University Electrocardiography Database (version 1.0.0). PhysioNet. (2020).
+            https://doi.org/10.13026/qweb-sr17.
+
     """
     lookup = {"N": 1, "A": 2}
 
@@ -64,11 +83,11 @@ def example_ecg(beat_idx: int = None) -> Tuple[np.ndarray, ...]:
     if beat_idx is None:
         label = np.zeros_like(recording, dtype=np.int32)
         for i_qrs in np.arange(len(sym))[1::3]:
-            label[samp[i_qrs - 1] : samp[i_qrs + 1]] = lookup[s] if (s := sym[i_qrs]) in lookup.keys() else 3
+            label[samp[i_qrs - 1]: samp[i_qrs + 1]] = lookup[s] if (s := sym[i_qrs]) in lookup.keys() else 3
         return recording, label
     else:
         assert beat_idx < n_qrs, f"The maximum value os beat_idx is {n_qrs}. But {beat_idx} was provided."
-        qrs_chunk = recording[samp[3 * beat_idx] : samp[3 * beat_idx + 2]]
+        qrs_chunk = recording[samp[3 * beat_idx]: samp[3 * beat_idx + 2]]
         sym = sym[3 * beat_idx + 1]
         qrs_loc = samp[3 * beat_idx + 1] - samp[3 * beat_idx]
         return qrs_chunk, qrs_loc, sym
