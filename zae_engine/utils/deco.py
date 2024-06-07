af96c9efd2c50ff2f5981ffb72bd597bd7b59b77
zae-park <tom941105@gmail.com>
2024-06-07 21:36:46 +0900

    doc: - optimize & add docstring for np2torch decorataor - optimize & add docstring for tictoc decorataor

--- zae_engine/utils/deco.py
+++ zae_engine/utils/deco.py
@@ -39,33 +39,84 @@ def np2torch(dtype: torch.dtype):
 
 
 def shape_check(*keys):
-    if len(keys) == 1:
-        keys = keys[0]
-        assert isinstance(keys, int), f"Input the num of args to check"
-        assert keys > 1, f"Cannot compare shape of single argument"
+    """
+    Ensure that the shapes of specified arguments are the same.
+
+    This decorator checks that the shapes of the specified arguments (either positional or keyword arguments) are the same.
+    If the shapes do not match, an AssertionError is raised.
+
+    Parameters
+    ----------
+    keys : int or str
+        If a single integer is provided, it checks the shapes of the first 'keys' positional arguments.
+        If multiple strings are provided, it checks the shapes of the corresponding keyword arguments.
+
+    Returns
+    -------
+    func
+        The decorated function with shape checks on the specified arguments.
+
+    Examples
+    --------
+    >>> @shape_check(2)
+    ... def example_func(x, y):
+    ...     return x + y
+    >>> @shape_check('x', 'y')
+    ... def example_func(**kwargs):
+    ...     return kwargs['x'] + kwargs['y']
+    """
+    if len(keys) == 1 and isinstance(keys[0], int):
+        num_args = keys[0]
+        if num_args <= 1:
+            raise AssertionError("Cannot compare shape of single argument")
     else:
         for k in keys:
-            assert isinstance(k, str), f"Input the multiple arg strings to check"
+            if not isinstance(k, str):
+                raise AssertionError("Input the multiple arg strings to check")
 
     def deco(func):
         def wrapper(*args, **kwargs):
-            if isinstance(keys, int):
-                shape_list = [a.shape for a in args[:keys]]
+            if isinstance(keys[0], int):
+                shape_list = [a.shape for a in args[:num_args]]
             else:
-                shape_list = [kwargs[key].shape for key in keys]
-            assert len(set(shape_list)) == 1, "Shape of given args is not same."
+                shape_list = [kwargs[key].shape for key in keys if key in kwargs]
+            if len(set(shape_list)) != 1:
+                raise AssertionError("Shape of given args is not same.")
             return func(*args, **kwargs)
-
         return wrapper
-
     return deco
 
 
+
 def tictoc(func):
+    """
+    Measure and print the elapsed time of a function execution.
+
+    This decorator measures the time it takes to execute the decorated function and prints the elapsed time in seconds.
+
+    Parameters
+    ----------
+    func : function
+        The function to be timed.
+
+    Returns
+    -------
+    func
+        The decorated function with timing functionality.
+
+    Examples
+    --------
+    >>> @tictoc
+    ... def example_func():
+    ...     time.sleep(1)
+    >>> example_func()  # This will print the elapsed time.
+    """
     def wrapper(*args, **kwargs):
+        import time
         kickoff = time.time()
         out = func(*args, **kwargs)
-        print(f"Elapsed time [sec]: {time.time() - kickoff}")
+        elapsed_time = time.time() - kickoff
+        print(f"Elapsed time [sec]: {elapsed_time}")
         return out
-
     return wrapper
+
