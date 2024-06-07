d54ff081e9792b1d6260be834f3a334cc35ac8a4
zae-park <tom941105@gmail.com>
2024-06-07 21:44:36 +0900

    test: fix test assertion

--- tests/loss/test_loss.py
+++ tests/loss/test_loss.py
@@ -36,10 +36,11 @@ class TestLoss(unittest.TestCase):
         # self.assertAlmostEqual(float(loss), 0.80567473, places=4)
 
     def test_batch_dot(self):
-        samples = torch.randn(size=(10, 256))
+        feat_len = randint(1, 512)
+        samples = torch.randn(size=(10, feat_len))
         dot_mat = batch_wise_dot(samples, reduce=False)
         self.assertLessEqual((1 - torch.diag(dot_mat)).mean(), utils.EPS)
-        self.assertLessEqual((torch.transpose(dot_mat, 0, 1) - dot_mat).mean(), utils.EPS)
+        self.assertLessEqual((torch.transpose(dot_mat, 0, 1) - dot_mat).mean(), utils.EPS * feat_len)
 
 
 if __name__ == "__main__":
