ed8e6c51812969fce17544d9a6f27a9e2a06018c
zae-park <tom941105@gmail.com>
2024-06-20 13:20:24 +0900

    doc: add docstring for GumbelSoftMax

--- zae_engine/nn_night/_gumbel_sotfmax.py
+++ zae_engine/nn_night/_gumbel_sotfmax.py
@@ -3,14 +3,36 @@ import torch
 
 class GumbelSoftMax(torch.autograd.Function):
     """
-    Re-parametric trick for categorical problems.
-    ref: https://blog.evjang.com/2016/11/tutorial-categorical-variational.html
-    TODO:
-        - fix description
-        - add test case
-        - optimize
+    Re-parametric trick for categorical problems using the Gumbel-Softmax trick.
+
+    The Gumbel-Softmax trick allows for sampling from a categorical distribution
+    in a differentiable manner, which is useful for incorporating categorical variables
+    into neural networks. This implementation follows the method described in [1]_.
+
+    Methods
+    -------
+    forward(ctx, *args)
+        Computes the forward pass, returning a rounded tensor while retaining
+        differentiability.
+
+    backward(ctx, *grad_outputs)
+        Computes the backward pass, returning the gradient of the input.
+
+    Examples
+    --------
+    >>> import torch
+    >>> tmp = torch.rand(10, dtype=torch.float64).clone().detach().requires_grad_(True)
+    >>> rounded = torch.round(tmp)
+    >>> stopped = tmp.detach()
+    >>> output = GumbelSoftMax.apply(tmp)
+    >>> output.backward(torch.ones_like(tmp))
+
+    References
+    ----------
+    .. [1] https://blog.evjang.com/2016/11/tutorial-categorical-variational.html
     """
 
+    # TODO: add test case & optimization
     @staticmethod
     def forward(ctx, *args):
         # Expect input argument to be a single tensor.
