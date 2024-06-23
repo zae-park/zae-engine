3e6e6d1be35532c9c5c38d96c5094fe149aae97e
zae-park <tom941105@gmail.com>
2024-06-23 11:29:23 +0900

    doc: add docstring for ResidualConnection

--- zae_engine/nn_night/layers/residual_connection.py
+++ zae_engine/nn_night/layers/residual_connection.py
@@ -3,18 +3,51 @@ import torch.nn as nn
 
 class Residual(nn.Sequential):
     """
-    Residual connection.
-    As nn.Sequential, receive modules but connect the output tensor to an input tensor.
+    Residual connection module.
+
+    This module extends nn.Sequential to implement a residual connection. The input tensor is added
+    to the output tensor of the sequence of modules provided during initialization, similar to a
+    residual block in ResNet architectures.
+
+    Parameters
+    ----------
+    *args : nn.Module
+        Sequence of PyTorch modules to be applied to the input tensor.
+
+    Methods
+    -------
+    forward(x)
+        Applies the sequence of modules to the input tensor and returns the sum of the input tensor
+        and the output tensor.
     """
 
     def __init__(self, *args):
+        """
+        Initialize the Residual module with a sequence of sub-modules.
+
+        Parameters
+        ----------
+        *args : nn.Module
+            Sequence of PyTorch modules to be applied to the input tensor.
+        """
         super(Residual, self).__init__(*args)
 
     def forward(self, x):
         """
-        The 'module' is sequence of arguments provides in __init__.
-        :param x: Input tensor
-        :return: Sum of input tensor and output of sequence.
+        Forward pass through the residual block.
+
+        Applies the sequence of modules to the input tensor and returns the sum of the input tensor
+        and the output tensor.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            The sum of the input tensor and the output of the sequence of modules.
         """
 
         residual = x
