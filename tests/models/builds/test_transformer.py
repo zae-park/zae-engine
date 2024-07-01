802d50e608b8fb6b6e2090afdfa38235dcad7850
zae-park <tom941105@gmail.com>
2024-07-01 13:04:47 +0900

    test: add more test cases - sequence length test - mask test

--- tests/models/builds/test_transformer.py
+++ tests/models/builds/test_transformer.py
@@ -19,6 +19,17 @@ class TestTimeAwareTransformer(unittest.TestCase):
         output = self.model(self.event_vecs, self.time_vecs)
         self.assertEqual(output.size(), (self.batch_size, self.d_model))
 
+    def test_variable_sequence_length(self):
+        event_vecs = torch.randn(5, self.batch_size, 128)
+        time_vecs = torch.randint(0, 512, (5, self.batch_size))
+        output = self.model(event_vecs, time_vecs)
+        self.assertEqual(output.size(), (self.batch_size, self.d_model))
+
+    def test_with_mask(self):
+        mask = torch.ones(self.batch_size, self.seq_len).bool()
+        output = self.model(self.event_vecs, self.time_vecs, mask)
+        self.assertEqual(output.size(), (self.batch_size, self.d_model))
+
 
 class TestUserIdentificationModel(unittest.TestCase):
     def setUp(self):
