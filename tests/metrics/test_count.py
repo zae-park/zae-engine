b17dc93b4e61c5f7e54510d97fd9d80b0b969801
zae-park <tom941105@gmail.com>
2024-07-14 02:49:57 +0900

    test: optimize assertion for countable metrics

--- tests/metrics/test_count.py
+++ tests/metrics/test_count.py
@@ -19,60 +19,34 @@ class TestCountMetrics(unittest.TestCase):
         self.assertAlmostEqual(accuracy(true_torch, predict_torch).item(), expected_accuracy, places=4)
 
     def test_f_beta_micro(self):
-        pred_np = np.array([1, 2, 3, 4])
-        true_np = np.array([1, 2, 2, 4])
-        pred_torch = torch.tensor([1, 2, 3, 4])
-        true_torch = torch.tensor([1, 2, 2, 4])
+        pred = np.array([1, 2, 3, 4])
+        true = np.array([1, 2, 2, 4])
 
-        expected_f_beta = 0.8
-
-        self.assertAlmostEqual(
-            f_beta(pred_np, true_np, beta=1.0, num_classes=5, average="micro").item(), expected_f_beta, places=4
-        )
-        self.assertAlmostEqual(
-            f_beta(pred_torch, true_torch, beta=1.0, num_classes=5, average="micro").item(), expected_f_beta, places=4
-        )
+        expected_f_beta = 0.75
+        computed_f_beta = f_beta(pred, true, beta=1.0, num_classes=5, average="micro")
+        self.assertAlmostEqual(computed_f_beta.item(), expected_f_beta, places=4)
 
     def test_f_beta_macro(self):
-        pred_np = np.array([0, 1, 2, 0])
-        true_np = np.array([0, 1, 1, 0])
-        pred_torch = torch.tensor([0, 1, 2, 0])
-        true_torch = torch.tensor([0, 1, 1, 0])
+        pred = np.array([0, 1, 2, 0])
+        true = np.array([0, 1, 1, 0])
 
-        expected_f_beta = 0.5
-
-        self.assertAlmostEqual(
-            f_beta(pred_np, true_np, beta=1.0, num_classes=3, average="macro").item(), expected_f_beta, places=4
-        )
-        self.assertAlmostEqual(
-            f_beta(pred_torch, true_torch, beta=1.0, num_classes=3, average="macro").item(), expected_f_beta, places=4
-        )
+        expected_f_beta = 0.55555558
+        computed_f_beta = f_beta(pred, true, beta=1.0, num_classes=3, average="macro")
+        self.assertAlmostEqual(computed_f_beta.item(), expected_f_beta, places=4)
 
     def test_f_beta_from_mat_micro(self):
-        conf_mat_np = np.array([[5, 2], [1, 3]])
-        conf_mat_torch = torch.tensor([[5, 2], [1, 3]])
+        conf_mat = np.array([[5, 2], [1, 3]])
 
         expected_f_beta = 0.7273
-
-        self.assertAlmostEqual(
-            f_beta_from_mat(conf_mat_np, beta=1.0, num_classes=2, average="micro").item(), expected_f_beta, places=4
-        )
-        self.assertAlmostEqual(
-            f_beta_from_mat(conf_mat_torch, beta=1.0, num_classes=2, average="micro").item(), expected_f_beta, places=4
-        )
+        computed_conf_mat = f_beta_from_mat(conf_mat, beta=1.0, num_classes=2, average="micro")
+        self.assertAlmostEqual(computed_conf_mat.item(), expected_f_beta, places=4)
 
     def test_f_beta_from_mat_macro(self):
-        conf_mat_np = np.array([[5, 2], [1, 3]])
-        conf_mat_torch = torch.tensor([[5, 2], [1, 3]])
+        conf_mat = np.array([[5, 2], [1, 3]])
 
-        expected_f_beta = 0.7083
-
-        self.assertAlmostEqual(
-            f_beta_from_mat(conf_mat_np, beta=1.0, num_classes=2, average="macro").item(), expected_f_beta, places=4
-        )
-        self.assertAlmostEqual(
-            f_beta_from_mat(conf_mat_torch, beta=1.0, num_classes=2, average="macro").item(), expected_f_beta, places=4
-        )
+        expected_f_beta = 0.71794867
+        computed_conv_mat = f_beta_from_mat(conf_mat, beta=1.0, num_classes=2, average="macro")
+        self.assertAlmostEqual(computed_conv_mat.item(), expected_f_beta, places=4)
 
 
 if __name__ == "__main__":
