3d3e3e060dc9ee0f3e8174427ca6d344fa388c40
zae-park <tom941105@gmail.com>
2024-08-02 14:50:27 +0900

    feat: add web_logger add-ons, `WandBLoggerAddon` and `NeptuneLoggerAddon`.
    - remove init args `web_logger` & `callbacks` from Trainer

--- zae_engine/trainer/addons/__init__.py
+++ zae_engine/trainer/addons/__init__.py
@@ -1,4 +1,5 @@
 from .core import AddOnBase
 from ._callback import *
-from ._logger import *
+from ._legacy_logger import *
 from .mpu import MultiGPUAddon
+from .web_logger import NeptuneLoggerAddon, WandBLoggerAddon
