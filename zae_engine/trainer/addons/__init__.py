0ccc9c3969e9a8ec3287906a4f46a436b008fa60
zae-park <tom941105@gmail.com>
2024-08-04 01:17:42 +0900

    remove the legacy

--- zae_engine/trainer/addons/__init__.py
+++ zae_engine/trainer/addons/__init__.py
@@ -1,6 +1,4 @@
 from .core import AddOnBase
-from ._callback import *
-from ._legacy_logger import *
 from .mpu import MultiGPUAddon
 from .web_logger import NeptuneLoggerAddon, WandBLoggerAddon
 from .state_manager import StateManagerAddon
