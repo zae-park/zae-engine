f3b854958241bf7bd15f82df356e761325f5d99e
zae-park <tom941105@gmail.com>
2024-08-13 08:51:51 +0000

    feat: update mpu add-on

--- zae_engine/trainer/addons/mpu.py
+++ zae_engine/trainer/addons/mpu.py
@@ -1,115 +1,98 @@
-from typing import Type, Tuple, Optional, Union, Sequence
-
+import os
+from typing import Type, Optional, List
+import copy
 import torch
+import torch.multiprocessing as mp
 import torch.utils.data as td
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DistributedSampler
-
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
 from .core import AddOnBase
 from .._trainer import T
 
+def train_process(rank, n_epoch, device_list, model_class, model_state_dict, trainer_args, trainer_kwargs, loader, valid_loader, **kwargs):
+    # Initialize distributed process group
+    dist.init_process_group(backend='nccl', init_method='env://', world_size=len(device_list), rank=rank)
+    torch.cuda.set_device(device_list[rank])
+
+    # Create a new model instance and load state dict
+    model = model_class()
+    model.load_state_dict(model_state_dict)
+    model = model.to(device_list[rank])
+    
+    # Wrap model with DDP
+    model = DDP(model, device_ids=[rank], output_device=rank)
+    
+    # Create Trainer instance for the current process
+    trainer = trainer_class(model=model, device=device_list[rank], *trainer_args, **trainer_kwargs)
+    
+    # Assign DistributedSampler to loaders
+    loader.sampler = DistributedSampler(loader.dataset, num_replicas=len(device_list), rank=rank)
+    if valid_loader:
+        valid_loader.sampler = DistributedSampler(valid_loader.dataset, num_replicas=len(device_list), rank=rank)
+    
+    # Run training
+    trainer.run(n_epoch, loader, valid_loader, **kwargs)
 
 class MultiGPUAddon(AddOnBase):
     """
-    Add-on for enabling multi-GPU support using Distributed Data Parallel (DDP) in the Trainer class.
+    Add-on for enabling multi-GPU support by distributing the model across multiple GPUs
+    and creating separate Trainer instances for each GPU.
     """
 
     @classmethod
     def apply(cls, base_cls: Type[T]) -> Type[T]:
         """
         Applies the multi-GPU modifications to the base class.
-
-        Parameters
-        ----------
-        base_cls : Type
-            The base class to which the multi-GPU modifications will be applied.
-
-        Returns
-        -------
-        Type
-            The modified base class with multi-GPU support.
         """
-
-        class MultiGPUTrainer(base_cls):
+        class MultiGPUTrainer:
             def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
-                self.rank = kwargs.pop("rank", 0)
-                self.world_size = kwargs.pop("world_size", 1)
-                self.setup_ddp(self.rank, self.world_size)
-                self.device = torch.device(f"cuda:{self.rank}")
-                self.model = DDP(self.model, device_ids=[self.rank], output_device=self.rank)
+                self.args, self.kwargs = args, kwargs
+                self.device_list = kwargs.pop("device", ["cuda:0"])
+                self.num_devices = len(self.device_list)
+                self.model = kwargs.pop("model", None)
+                
+                # Save the class and arguments to create Trainer instances later
+                self.trainer_class = base_cls
+                self.trainer_args = args
+                self.trainer_kwargs = kwargs
+                self.model_state_dict = self.model.state_dict()
 
-            def _set_device(self, device: Union[torch.device, Sequence[torch.device]]):
-                # Device is set up already in __init__
-                pass
-
-            def _to_device(
-                self, *args, **kwargs
-            ) -> Tuple[Union[torch.Tensor, torch.nn.Module]] or Union[torch.Tensor, torch.nn.Module]:
+            def _create_trainer_for_device(self, device):
                 """
-                Cast given arguments to device.
-
-                Parameters
-                ----------
-                args : Single argument or variable-length sequence of arguments.
-                    The arguments to be cast to the specified device.
-
-                Returns
-                -------
-                Single argument or variable-length sequence of arguments in device.
+                Create a trainer instance for a specific device.
                 """
-                if isinstance(self.device, torch.device):
-                    if args:
-                        if len(args) == 1:
-                            return args[0].to(self.device) if "to" in args[0].__dir__() else args[0]
-                        else:
-                            return tuple([a.to(self.device) if "to" in a.__dir__() else a for a in args])
-                    elif kwargs:
-                        return {k: v.to(self.device) if "to" in v.__dir__() else v for k, v in kwargs.items()}
-                else:
-                    # Handle multiple GPUs
-                    for d in self.device:
-                        if args:
-                            if len(args) == 1:
-                                return args[0].to(d) if "to" in args[0].__dir__() else args[0]
-                            else:
-                                return tuple([a.to(d) if "to" in a.__dir__() else a for a in args])
-                        elif kwargs:
-                            return {k: v.to(d) if "to" in v.__dir__() else v for k, v in kwargs.items()}
+                # Deep copy the model and load the state dict
+                model_copy = copy.deepcopy(self.model)
+                model_copy.load_state_dict(self.model_state_dict)
+                model_copy = model_copy.to(device)
 
-            def setup_ddp(self, rank, world_size):
-                dist.init_process_group(backend="nccl", init_method="env://", world_size=world_size, rank=rank)
-                torch.cuda.set_device(rank)
+                # Create and return Trainer instance
+                trainer = self.trainer_class(model=model_copy, device=device, *self.trainer_args, **self.trainer_kwargs)
+                return trainer
 
-            def cleanup_ddp(self):
-                dist.destroy_process_group()
-
-            def run(
-                self, n_epoch: int, loader: td.DataLoader, valid_loader: Optional[td.DataLoader] = None, **kwargs
-            ) -> None:
+            def run(self, n_epoch, loader: td.DataLoader, valid_loader: Optional[td.DataLoader] = None, **kwargs):
                 """
-                Run the training/testing process for a given number of epochs with DistributedSampler.
-
-                Parameters
-                ----------
-                n_epoch : int
-                    The number of epochs to run.
-                loader : td.DataLoader
-                    The data loader for the training/testing data.
-                valid_loader : Optional[td.DataLoader], optional
-                    The data loader for validation data, by default None.
+                Run training/testing process on all GPUs.
                 """
-                # Assign DistributedSampler to loaders
-                loader.sampler = DistributedSampler(loader.dataset, num_replicas=self.world_size, rank=self.rank)
-                if valid_loader:
-                    valid_loader.sampler = DistributedSampler(
-                        valid_loader.dataset, num_replicas=self.world_size, rank=self.rank
-                    )
+                # Prepare arguments for train_process
+                train_kwargs = {
+                    'device_list': self.device_list,
+                    'model_class': self.model.__class__,
+                    'model_state_dict': self.model_state_dict,
+                    'trainer_args': self.trainer_args,
+                    'trainer_kwargs': self.trainer_kwargs,
+                    'loader': loader,
+                    'valid_loader': valid_loader,
+                    **kwargs
+                }
 
-                super().run(n_epoch, loader, valid_loader, **kwargs)
-
-            def __del__(self):
-                self.cleanup_ddp()
+                # Use multiprocessing to run training on each device
+                mp.spawn(
+                    train_process,
+                    args=(n_epoch, self.device_list, self.model.__class__, self.model_state_dict, self.trainer_args, self.trainer_kwargs, loader, valid_loader, kwargs),
+                    nprocs=self.num_devices,
+                    join=True
+                )
 
         return MultiGPUTrainer
