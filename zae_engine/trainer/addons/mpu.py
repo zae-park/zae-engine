ce55b6d49bfa2ca786908f2d5120e8eedd32b08d
zae-park <tom941105@gmail.com>
2024-08-14 01:14:53 +0000

    feat: update mpu add-on

--- zae_engine/trainer/addons/mpu.py
+++ zae_engine/trainer/addons/mpu.py
@@ -1,25 +1,38 @@
 import os
-import copy
-from typing import Type, Optional, List
+import tempfile
+from typing import Type, Union, Optional, Dict
+
 import torch
-import torch.multiprocessing as mp
 import torch.utils.data as td
+import torch.distributed as dist
+import torch.multiprocessing as mp
 from torch.utils.data import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.distributed as dist
+
 from .core import AddOnBase
 from .._trainer import T
 
+
 def train_process(rank, device_list, model_class, model_state_dict, trainer_class, trainer_args, trainer_kwargs, n_epoch, loader, valid_loader, aux_run_kwargs):
-    print('1:', rank, device_list)
-    print('2:', model_class, model_state_dict)
-    print('3:', trainer_class, trainer_args, trainer_kwargs)
-    print('4:', n_epoch, loader, valid_loader, aux_run_kwargs)
+    print(f'Initializing process {rank}...')
+    
+    # Create a temporary file to use for the file-based rendezvous
+    init_file = tempfile.NamedTemporaryFile(delete=True)
+    init_file.close()  # We close the file so that other processes can access it
+    
+    # Set the environment variables for distributed training
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    os.environ['WORLD_SIZE'] = str(len(device_list))
+    os.environ['RANK'] = str(rank)
+    
+    print(f'Process {rank} initializing process group...')
     # Initialize distributed process group
-    dist.init_process_group(backend='nccl', init_method='env://', world_size=len(device_list), rank=rank)
+    dist.init_process_group(backend='nccl', init_method=f'file://{init_file.name}', world_size=len(device_list), rank=rank)
     torch.cuda.set_device(device_list[rank])
 
     # Create a new model instance and load state dict
+    print(f'Process {rank} loading model...')
     model = model_class()
     model.load_state_dict(model_state_dict)
     model = model.to(device_list[rank])
@@ -31,12 +44,35 @@ def train_process(rank, device_list, model_class, model_state_dict, trainer_clas
     trainer = trainer_class(model=model, device=device_list[rank], *trainer_args, **trainer_kwargs)
     
     # Assign DistributedSampler to loaders
-    loader.sampler = DistributedSampler(loader.dataset, num_replicas=len(device_list), rank=rank)
+    print(f'Process {rank} setting up data loaders...')
+    loader_sampler = DistributedSampler(loader.dataset, num_replicas=len(device_list), rank=rank)
+    train_loader_mp = td.DataLoader(
+        loader.dataset,
+        batch_size=loader.batch_size,
+        shuffle=False,  # shuffle=False since DistributedSampler handles shuffling
+        num_workers=loader.num_workers,
+        pin_memory=loader.pin_memory,
+        drop_last=loader.drop_last,
+        sampler=loader_sampler
+    )
+    
+    valid_loader_mp = None
     if valid_loader:
-        valid_loader.sampler = DistributedSampler(valid_loader.dataset, num_replicas=len(device_list), rank=rank)
+        valid_loader_sampler = DistributedSampler(valid_loader.dataset, num_replicas=len(device_list), rank=rank)
+        valid_loader_mp = td.DataLoader(
+            valid_loader.dataset,
+            batch_size=valid_loader.batch_size,
+            shuffle=False,  # shuffle=False since DistributedSampler handles shuffling
+            num_workers=valid_loader.num_workers,
+            pin_memory=valid_loader.pin_memory,
+            drop_last=valid_loader.drop_last,
+            sampler=valid_loader_sampler
+        )
     
     # Run training
-    trainer.run(n_epoch, loader, valid_loader, **aux_run_kwargs)
+    print(f'Process {rank} running training...')
+    trainer.run(n_epoch, train_loader_mp, valid_loader_mp, **aux_run_kwargs)
+
 
 class MultiGPUAddon(AddOnBase):
     """
