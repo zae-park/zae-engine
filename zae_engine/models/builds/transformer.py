9c0408d98efe1184590635cbcdfcb7829c021544
zae-park <tom941105@gmail.com>
2024-09-10 18:41:40 +0900

    feat: update weight mapper

--- zae_engine/models/builds/transformer.py
+++ zae_engine/models/builds/transformer.py
@@ -340,6 +340,80 @@ class DecoderBase(CoderBase):
         return output
 
 
+class Additional(nn.ModuleList):
+    """
+    Additional connection module.
+
+    This module extends nn.ModuleList to implement an additional connection.
+    Each input tensor is passed through its corresponding module,
+    and the output tensors are summed. If the shapes of the output tensors
+    do not match, an error is raised.
+
+    Parameters
+    ----------
+    *args : nn.Module
+        Sequence of PyTorch modules. Each module will be applied to
+        a corresponding input tensor in the forward pass.
+
+    Methods
+    -------
+    forward(*inputs)
+        Applies each module to its corresponding input tensor and returns
+        the sum of the output tensors. If the shapes of the output tensors
+        do not match, an error is raised.
+    """
+
+    def __init__(self, *args):
+        """
+        Initialize the Additional module with a sequence of sub-modules.
+
+        Parameters
+        ----------
+        *args : nn.Module
+            Sequence of PyTorch modules to be applied to the input tensors.
+        """
+        super(Additional, self).__init__(args)
+
+    def forward(self, *inputs):
+        """
+        Forward pass through the additional block.
+
+        Applies each module to its corresponding input tensor and returns
+        the sum of the output tensors. If the shapes of the output tensors
+        do not match, an error is raised.
+
+        Parameters
+        ----------
+        *inputs : torch.Tensor
+            Sequence of input tensors. Each tensor is passed through its corresponding module.
+
+        Returns
+        -------
+        torch.Tensor
+            The sum of the output tensors of each module.
+
+        Raises
+        ------
+        ValueError
+            If the output tensors have mismatched shapes.
+        """
+
+        if len(inputs) != len(self):
+            raise ValueError(f"Expected {len(self)} input tensors, but got {len(inputs)}.")
+
+        # Apply each module to its corresponding input and store the outputs in a list
+        outputs = [layer(inputs[i]) for i, layer in enumerate(self)]
+
+        # Ensure that all output tensors have the same shape
+        first_shape = outputs[0].shape
+        for output in outputs[1:]:
+            if output.shape != first_shape:
+                raise ValueError(f"Shape mismatch: expected {first_shape}, but got {output.shape}")
+
+        # Return the sum of the output tensors
+        return sum(outputs)
+
+
 def __weight_mapper(src_weight: [dict], dst_weight: [dict]):
     """
     Map source weights to destination model weights.
@@ -357,30 +431,60 @@ def __weight_mapper(src_weight: [dict], dst_weight: [dict]):
         Updated destination model weights.
     """
 
+    buff_dict = {}
+
     for k, v in src_weight.items():
-
-        # Aggregate QKV projection to in_proj layer
-        # Bert Encoder -> Zae Encoder
-
         if k.startswith("embeddings"):
             k = k.replace("embeddings", "encoder_embedding")
             k = (
-                k.replace("word_embeddings", "word")
-                .replace("position_embeddings", "position")
-                .replace("token_type", "type")
+                k.replace("word_embeddings", "word")  # 0
+                .replace("position_embeddings", "position")  # 1
+                .replace("token_type", "type")  # 2
             )
             k = k.replace("LayerNorm", "norm")
-        elif k.startswith("encoder"):
-            # From : encoder.layer.0.attention.self.query.weight
-            # To : encoder.layers.0.self_attn.in_proj_weight
-            pass
+        elif k.startswith("encoder.layer"):
+            # Save QKV weight & bias to buffer
+            if "attention.self" in k:
+                tkn = k.split(".")
+                n_layer = tkn[2]
+                para_name = "_".join(tkn[-2:])
+                buff_dict[f"{n_layer}_{para_name}"] = v
+
+            elif "attention.output" in k:
+                k = (
+                    k.replace("layer.", "layers.")
+                    .replace("attention.output", "self_attn")
+                    .replace("dense", "out_proj")
+                    .replace("LayerNorm", "norm")
+                )
+
+            elif "intermediate" in k:
+                k = k.replace("intermediate.", "").replace("dense", "linear1")
+
+            elif "output" in k:
+                k = k.replace("output,", "").replace("dense", "linear2").replace("LayerNorm", "norm1")
+                asd = "dense"
+                asd = "LayerNorm"
+
+            else:
+                print(k)
+
         elif k.startswith("pooler"):
             pass
         else:
-            k = "stem." + k
-            k = k.replace("conv1", "0").replace("bn1", "1")
+            print(k)
 
         dst_weight[k] = v
+
+    # Generate in_proj weight & bias using theirs of QKV in buffer
+    total_layer = len(buff_dict) // 6
+    for t in total_layer:
+        qkv_w = [buff_dict[f"{t}_{n}_weight"] for n in ["query", "key", "value"]]
+        qkv_b = [buff_dict[f"{t}_{n}_bias"] for n in ["query", "key", "value"]]
+        dst_weight[f"encoder.layers.{t}.self_attn.in_proj_weight"] = torch.cat(qkv_w, dim=0)
+        dst_weight[f"encoder.layers.{t}.self_attn.in_proj_bias"] = torch.cat(qkv_b, dim=0)
+        # From : encoder.layer.0.attention.self.query.weight
+        # To : encoder.layers.0.self_attn.in_proj_weight
 
     return dst_weight
 
@@ -397,12 +501,19 @@ if __name__ == "__main__":
     bert_pool = pre_model.pooler  # BertPooler : Dense(768, 768) + Tanh()
 
     # Embedding = word + positional + type
-    zae_emb = nn.ModuleList([nn.Embedding(30522, 768, padding_idx=0), nn.Embedding(512, 768), nn.Embedding(2, 768)])
-    zae_embedding = nn.Sequential(nn.Embedding(30522 + 512 + 2, 768, padding_idx=0), nn.LayerNorm(768), nn.Dropout(0.1))
+
+    zae_emb = nn.Sequential(
+        Additional(
+            nn.Embedding(30522, 768, padding_idx=0),
+            nn.Embedding(512, 768),
+            nn.Embedding(2, 768),
+        ),
+        nn.LayerNorm(768),
+    )
 
     encoder = EncoderBase(d_model=768, num_layers=12, layer_factory=nn.TransformerEncoderLayer, dim_feedforward=3072)
     decoder = nn.Identity()
-    model = TransformerBase(encoder_embedding=zae_embedding, encoder=encoder)
+    model = TransformerBase(encoder_embedding=zae_emb, encoder=encoder)
 
     enc = __weight_mapper(pre_model.state_dict(), model.state_dict())
 
