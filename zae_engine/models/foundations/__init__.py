d0cf157e690ee36f86820a8e87e5022809ebb7ab
zae-park <tom941105@gmail.com>
2024-09-13 17:17:38 +0900

    optimize module import

--- zae_engine/models/foundations/__init__.py
+++ zae_engine/models/foundations/__init__.py
@@ -1,7 +1,7 @@
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .resnet import seresnet18, seresnet34, seresnet50, seresnet101, seresnet152
 from .resnet import cbamresnet18, cbamresnet34, cbamresnet50, cbamresnet101, cbamresnet152
-from .unet import unet
+from .unet import unet_brain
 from .bert import bert_base
 from .benchmark import UserIdModel
 
