a87148bbb5a481af0684e5659283baa4bdca5824
zae-park <tom941105@gmail.com>
2024-09-17 03:56:17 +0900

    feat: UserIdModel with EncoderBase

--- tests/models/foundations/test_userIDnet.py
+++ tests/models/foundations/test_userIDnet.py
@@ -1,54 +1,54 @@
-# import unittest
-# import torch
-#
-# from zae_engine.models import UserIdModel
-#
-#
-# class TestUserIdModel(unittest.TestCase):
-#     def setUp(self):
-#         self.d_head = 32
-#         self.d_model = 128
-#         self.n_head = 8
-#         self.num_layers = 6
-#         self.num_classes = 1000
-#         self.model = UserIdModel(
-#             d_head=self.d_head,
-#             d_model=self.d_model,
-#             n_head=self.n_head,
-#             num_layers=self.num_layers,
-#             num_classes=self.num_classes,
-#         )
-#         self.batch_size = 16
-#         self.seq_len = 10
-#         self.event_vecs = torch.randn(self.batch_size, self.seq_len, self.d_head)
-#         self.time_vecs = torch.randint(0, 512, (self.batch_size, self.seq_len))
-#         self.labels = torch.randint(0, self.num_classes, (self.batch_size,))
-#
-#     def test_forward(self):
-#         logits, features = self.model(self.event_vecs, self.time_vecs, self.labels)
-#         self.assertEqual(logits.size(), (self.batch_size, self.num_classes))
-#         self.assertEqual(features.size(), (self.batch_size, self.d_model))
-#
-#     def test_expand_classes(self):
-#         new_num_classes = 1100
-#         self.model.expand_classes(new_num_classes)
-#         self.assertEqual(self.model.num_classes, new_num_classes)
-#         self.assertEqual(self.model.arcface.weight.size(), (new_num_classes, self.d_model))
-#
-#     def test_forward_after_expand_classes(self):
-#         new_num_classes = 1100
-#         self.model.expand_classes(new_num_classes)
-#         logits, features = self.model(self.event_vecs, self.time_vecs, self.labels)
-#         self.assertEqual(logits.size(), (self.batch_size, new_num_classes))
-#         self.assertEqual(features.size(), (self.batch_size, self.d_model))
-#
-#     def test_variable_sequence_length(self):
-#         event_vecs = torch.randn(self.batch_size, 5, self.d_head)
-#         time_vecs = torch.randint(0, 512, (self.batch_size, 5))
-#         logits, features = self.model(event_vecs, time_vecs, self.labels)
-#         self.assertEqual(logits.size(), (self.batch_size, self.num_classes))
-#         self.assertEqual(features.size(), (self.batch_size, self.d_model))
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
+import unittest
+import torch
+
+from zae_engine.models import UserIdModel
+
+
+class TestUserIdModel(unittest.TestCase):
+    def setUp(self):
+        self.d_head = 32
+        self.d_model = 128
+        self.n_head = 8
+        self.num_layers = 6
+        self.num_classes = 1000
+        self.model = UserIdModel(
+            d_head=self.d_head,
+            d_model=self.d_model,
+            n_head=self.n_head,
+            num_layers=self.num_layers,
+            num_classes=self.num_classes,
+        )
+        self.batch_size = 16
+        self.seq_len = 10
+        self.event_vecs = torch.randn(self.batch_size, self.seq_len, self.d_head)
+        self.time_vecs = torch.randint(0, 512, (self.batch_size, self.seq_len))
+        self.labels = torch.randint(0, self.num_classes, (self.batch_size,))
+
+    def test_forward(self):
+        logits, features = self.model(self.event_vecs, self.time_vecs, self.labels)
+        self.assertEqual(logits.size(), (self.batch_size, self.num_classes))
+        self.assertEqual(features.size(), (self.batch_size, self.d_model))
+
+    def test_expand_classes(self):
+        new_num_classes = 1100
+        self.model.expand_classes(new_num_classes)
+        self.assertEqual(self.model.num_classes, new_num_classes)
+        self.assertEqual(self.model.arcface.weight.size(), (new_num_classes, self.d_model))
+
+    def test_forward_after_expand_classes(self):
+        new_num_classes = 1100
+        self.model.expand_classes(new_num_classes)
+        logits, features = self.model(self.event_vecs, self.time_vecs, self.labels)
+        self.assertEqual(logits.size(), (self.batch_size, new_num_classes))
+        self.assertEqual(features.size(), (self.batch_size, self.d_model))
+
+    def test_variable_sequence_length(self):
+        event_vecs = torch.randn(self.batch_size, 5, self.d_head)
+        time_vecs = torch.randint(0, 512, (self.batch_size, 5))
+        logits, features = self.model(event_vecs, time_vecs, self.labels)
+        self.assertEqual(logits.size(), (self.batch_size, self.num_classes))
+        self.assertEqual(features.size(), (self.batch_size, self.d_model))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- zae_engine/models/foundations/benchmark.py
+++ zae_engine/models/foundations/benchmark.py
@@ -1,106 +1,7 @@
-from typing import Optional
-
 import torch.nn as nn
 
 from ..builds import transformer as trx
 from ...loss import angular
-
-#
-# from ..builds.legacy import Segmentor, Regressor1D
-# from ..utility import initializer, WeightLoader
-#
-#
-# def beat_segmentation(pretrained: Optional[bool] = False) -> nn.Module:
-#     """
-#     Build model which has a same structure with the latest released model.
-#     :param pretrained: bool
-#         If True, load weight from the server.
-#         If not, weights are initialized with 'initializer' method in utils.py
-#     :return: nn.Module
-#     """
-#     model = Segmentor(
-#         ch_in=1,
-#         ch_out=4,
-#         width=16,
-#         kernel_size=7,
-#         depth=5,
-#         order=4,
-#         stride=(2, 2, 2, 2),
-#         decoding=False,
-#     )
-#
-#     if pretrained:
-#         weights = WeightLoader.get("beat_segmentation")
-#         model.load_state_dict(weights, strict=True)
-#     else:
-#         model.apply(initializer)
-#
-#     return model
-#
-#
-# def peak_regression(pretrained: Optional[bool] = False) -> nn.Module:
-#     """
-#     Build model which has a same structure with the latest released model.
-#     :param pretrained: bool
-#         If True, load weight from the server.
-#         If not, weights are initialized with 'initializer' method in utils.py.
-#     :return: nn.Module
-#     """
-#     model = Regressor1D(
-#         dim_in=64,
-#         ch_in=1,
-#         width=16,
-#         kernel_size=3,
-#         depth=2,
-#         stride=1,
-#         order=4,
-#         head_depth=1,
-#         embedding_dims=16,
-#     )
-#
-#     if pretrained:
-#         weights = WeightLoader.get("peak_regression")
-#         model.load_state_dict(weights, strict=True)
-#     else:
-#         model.apply(initializer)
-#
-#     return model
-#
-#
-# def u_net(pretrained: Optional[bool] = False) -> nn.Module:
-#     model = Segmentor(
-#         ch_in=1,
-#         ch_out=2,
-#         width=64,
-#         kernel_size=[3, 3],
-#         depth=5,
-#         order=2,
-#         stride=(2, 2, 2, 2),
-#         decoding=True,
-#     )
-#     if pretrained:
-#         weights = WeightLoader.get("u_net")
-#         model.load_state_dict(weights, strict=True)
-#
-#     return model
-#
-#
-# # if __name__ == "__main__":
-# #     import numpy as np
-# #
-# #     m = u_net()
-# #     tmp = np.zeros((10, 1, 256, 256), dtype=np.float32)
-# #     tmp = torch.Tensor(tmp)
-# #     p = m(tmp)
-# #     print(m)
-# #     print(p)
-# #
-# #     m = u_net1()
-# #     tmp = np.zeros((10, 1, 256), dtype=np.float32)
-# #     tmp = torch.Tensor(tmp)
-# #     p = m(tmp)
-# #     print(m)
-# #     print(p)
 
 
 class UserIdModel(nn.Module):
@@ -128,9 +29,7 @@ class UserIdModel(nn.Module):
 
     def __init__(self, d_head, d_model, n_head, num_layers, num_classes):
         super(UserIdModel, self).__init__()
-        self.transformer = trx.TimeAwareTransformer(
-            d_head=d_head, d_model=d_model, n_head=n_head, num_layers=num_layers
-        )
+        self.transformer = trx.EncoderBase(d_model=d_model, num_layers=num_layers)
         self.arcface = angular.ArcFaceLoss(d_model, num_classes)
         self.num_classes = num_classes
 
