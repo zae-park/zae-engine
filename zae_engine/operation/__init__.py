4773acef40f1c335ca21f78f0bb17b184ac7fa27
zae-park <tom941105@gmail.com>
2024-10-01 21:10:08 +0900

    feat: add initial RLD(Run-Length Decoding)

--- zae_engine/operation/__init__.py
+++ zae_engine/operation/__init__.py
@@ -3,5 +3,5 @@ from ._operation import onoff_to_label
 from ._operation import label_to_onoff
 from ._operation import onoff_to_label
 from ._operation import find_nearest
-from ._operation import run_length_encoding, Run
+from ._operation import run_length_encoding, run_length_decoding, Run
 from ._operation import MorphologicalLayer
