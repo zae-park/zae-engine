936b2a6449af340ed27fa089efabd602b380a8b3
zae-park <tom941105@gmail.com>
2024-10-02 13:27:22 +0900

    feat: remove Run Length related functions

--- zae_engine/operation/__init__.py
+++ zae_engine/operation/__init__.py
@@ -3,5 +3,4 @@ from ._operation import onoff_to_label
 from ._operation import label_to_onoff
 from ._operation import onoff_to_label
 from ._operation import find_nearest
-from ._operation import run_length_encoding, run_length_decoding, Run
 from ._operation import MorphologicalLayer
