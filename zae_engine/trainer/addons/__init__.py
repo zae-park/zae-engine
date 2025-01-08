bbfe3142566137aa21b67d17a1eae6188f064ac2
zae-park <tom941105@gmail.com>
2025-01-08 11:32:31 +0900

    optimize module import

--- zae_engine/trainer/addons/__init__.py
+++ zae_engine/trainer/addons/__init__.py
@@ -2,3 +2,4 @@ from .core import AddOnBase
 from .mpu import MultiGPUAddon
 from .web_logger import NeptuneLoggerAddon, WandBLoggerAddon
 from .state_manager import StateManagerAddon
+from .mix_precision import PrecisionMixerAddon
